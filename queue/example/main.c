6e1900b473dda118183f4bb58c1082c57262dc1c
Vincent ASTOLFI <100098145+viastolfi@users.noreply.github.com>
2025-04-23 09:44:06 +0200

    fix: example

--- queue/example/main.c
+++ queue/example/main.c
@@ -1,4 +1,5 @@
 #define QUEUE_LIB_IMPLEMENTATION
+#define QUEUE_NO_THREADING
 #include "../queue.h"
 
 #include <stdio.h>
@@ -10,12 +11,23 @@ int main(void)
     char* b = "bar";
     char* c = "baz";
 
-    queue_t* queue = queue_init(); 
-    queue_add(queue, a);
-    queue_add(queue, b);
-    queue_add(queue, c);
+    queue_t* queue;
+    if((queue = queue_init()) == NULL)
+    {
+        perror("error on init");
+        exit(errno);
+    }
 
-    char* i = malloc(4);
+    if( 
+        !queue_add(queue, a) ||
+        !queue_add(queue, b) ||
+        !queue_add(queue, c))
+    {
+        perror("error on add");
+        exit(errno);
+    }
+
+    char* i;
     if((i = (char*) queue_dequeue(queue)) == NULL)
     {
         perror("error on dequeue");
@@ -24,5 +36,12 @@ int main(void)
 
     // expected foo
     printf("value in dequeued : %s\n", i);
+
+    if(!queue_free(queue))
+    {
+        perror("error in free");
+        exit(errno);
+    }
+    
     return 0;
 }
