7fd7a00beea3078b9d1735005a6a1565111e3182
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-04-26 18:28:45 +0200

    feat: da example

---
+++ dynamic_array/example/main.c
@@ -0,0 +1,25 @@
+#define DA_LIB_IMPLEMENTATION
+#include "../da.h"
+
+#include <stdio.h>
+
+typedef struct
+{
+    int* items;
+    size_t count;
+    size_t capacity;
+} Numbers;
+
+int main(void) 
+{
+    Numbers numbers = {0};    
+
+    da_append(&numbers, 1);
+    da_append(&numbers, 2);
+    da_append(&numbers, 3);
+
+    da_foreach(int, it, &numbers) {
+        size_t index = it - numbers.items;
+        printf("%zu: %d\n", index, *it); 
+    }
+}
