25e336b7a2309d86a30ccc45adf83d38f60bc027
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-11-05 11:23:16 +0100

    feat(ctest): enhance console output

--- ctest/ctest.h
+++ ctest/ctest.h
@@ -9,6 +9,12 @@
 #define COLOR_SUCCESS "\033[32m"
 #define COLOR_FAIL "\033[31m"
 #define COLOR_HINT "\033[33m"
+#define COLOR_BOLD  "\e[1m"
+#define BOLD_OFF   "\e[m"
+#define COLOR_SUM "\033[36m"
+
+static int total = 0;
+static int success = 0;
 
 typedef void (*ct_func)(void);
 
@@ -41,14 +47,71 @@ static void add_test(ct_test* t) {
   static void register_##SUITE##_##NAME(void) { add_test(&ct_test_##SUITE##_##NAME); } \
   static void SUITE##_##NAME##_impl(void) 
 
-#define ct_assert(expr) \
+#define ct_assert(expr, message) \
   do { \
-    printf("(%s:%d) ", __FILE__, __LINE__); \
-    if (!expr) \
+    total++; \
+    if (!expr) { \
       printf(COLOR_FAIL "[FAIL] " COLOR_RESET); \
-    else \
+      printf("(%s:%d) ", __FILE__, __LINE__); \
+      printf("%s\n", message); \
+      printf(COLOR_HINT "[HINT] " COLOR_RESET); \
+      printf(COLOR_SUM COLOR_BOLD "expected :" COLOR_RESET " true\n"); \
+      printf(COLOR_HINT "[HINT] " COLOR_RESET); \
+      printf(COLOR_SUM COLOR_BOLD "result   :" COLOR_RESET " false\n"); \
+    } else {\
       printf(COLOR_SUCCESS "[PASS] " COLOR_RESET); \
+      printf("(%s:%d) ", __FILE__, __LINE__); \
+      printf("%s\n", message); \
+      success++; \
+    } \
   } while (0) 
+
+static void ct_assert_eq_int(int x, int y, const char* message, const char* file, int line) 
+{
+  total++;
+  if (x == y) {
+    printf(COLOR_SUCCESS "[PASS] " COLOR_RESET);
+    printf("(%s:%d) ", file, line); 
+    printf("%s\n", message);
+    success++; 
+  }
+  else {
+    printf(COLOR_FAIL "[FAIL] " COLOR_RESET);
+    printf("(%s:%d) ", file, line); 
+    printf("%s", message);
+    printf(COLOR_HINT "\n[HINT] " COLOR_RESET);
+    printf(COLOR_BOLD COLOR_SUM "expected : " COLOR_RESET BOLD_OFF "%d\n", x);
+    printf(COLOR_HINT "[HINT] " COLOR_RESET);
+    printf(COLOR_BOLD COLOR_SUM "result   : " COLOR_RESET BOLD_OFF "%d\n", y);
+  }
+}
+
+static void ct_assert_eq_string(const char* x, const char* y, const char* message, const char* file, int line)
+{
+  total++;
+  if (strcmp(x, y) == 0) {
+    printf(COLOR_SUCCESS "[PASS] " COLOR_RESET);
+    printf("(%s:%d) ", file, line);
+    printf("%s\n", message);
+    success++; 
+  }
+  else {
+    printf(COLOR_FAIL "[FAIL] " COLOR_RESET);
+    printf("(%s:%d) ", file, line); 
+    printf("%s", message);
+    printf(COLOR_HINT "\n[HINT] " COLOR_RESET);
+    printf(COLOR_BOLD COLOR_SUM "expected : " COLOR_RESET BOLD_OFF "\"%s\"\n", x);
+    printf(COLOR_HINT "[HINT] " COLOR_RESET);
+    printf(COLOR_BOLD COLOR_SUM "result   : " COLOR_RESET BOLD_OFF "\"%s\"\n", y);
+  }
+}
+
+#define ct_assert_eq(x, y, message) \
+  _Generic((x), \
+    int: ct_assert_eq_int, \
+    char*: ct_assert_eq_string, \
+    const char*: ct_assert_eq_string \
+  )(x, y, message, __FILE__, __LINE__)
 
 int main(void) 
 {
@@ -68,11 +131,12 @@ int main(void)
 
   t = reversed;
   while (t) {
-    printf("[----] %s::%s\n", t->suite, t->name);
+    printf("[" COLOR_SUM "----" COLOR_RESET "] " COLOR_BOLD "%s::%s" BOLD_OFF"\n", t->suite, t->name);
     t->func();
-    printf("%s::%s\n", t->suite, t->name); 
     t = t->next;
   }
+
+  printf("[" COLOR_SUM "====" COLOR_RESET "] " COLOR_BOLD "Summary : " COLOR_HINT "%d " COLOR_RESET COLOR_BOLD "Test | " COLOR_SUCCESS "%d " COLOR_RESET COLOR_BOLD "Success | " COLOR_FAIL "%d " COLOR_RESET COLOR_BOLD "Fail\n" BOLD_OFF, total, success, total - success);
 
   return 0;
 }
--- ctest/example/main.c
+++ ctest/example/main.c
@@ -2,10 +2,22 @@
 
 ct_test(simple, test) 
 {
-  ct_assert(1);
+  ct_assert(1, "success test");
 }
 
 ct_test(simple, fail)
 {
-  ct_assert(0);
+  ct_assert(0, "fail test");
+}
+
+ct_test(eq, test)
+{
+  ct_assert_eq(1, 1, "success int eq test");
+  ct_assert_eq("test", "test", "success string eq test");
+}
+
+ct_test(eq, test_fail)
+{
+  ct_assert_eq(1, 2, "fail int eq test");
+  ct_assert_eq("test", "fail", "fail string eq test");
 }
